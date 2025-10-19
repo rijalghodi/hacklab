0a976fd6c5436deec0d8cee1ae8c9be2e8da2879
Rijal <rijalghodi.dev@gmail.com>
2025-10-19 18:25:17 +0700

    wip: add tooltip in sidebar toggle

--- components/flow/flow-sidebar/flow-sidebar-trigger.tsx
+++ components/flow/flow-sidebar/flow-sidebar-trigger.tsx
@@ -2,20 +2,27 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
-import { SidebarTrigger, useSidebar } from "../../ui/sidebar";
+import { SidebarTrigger, Tooltip, TooltipContent, TooltipTrigger, useSidebar } from "@/components/ui";
 
 export function FlowSidebarTrigger(props: { className?: string }) {
   const { toggleSidebar, open } = useSidebar();
 
   return (
-    <SidebarTrigger
-      className={cn("h-12 w-7", props.className)}
-      variant="ghost"
-      size="icon"
-      onClick={toggleSidebar}
-      title="Toggle Chip Menu"
-    >
-      {open ? <ChevronLeft className="size-6" /> : <ChevronRight className="size-6" />}
-    </SidebarTrigger>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <SidebarTrigger
+          className={cn("h-12 w-7", props.className)}
+          variant="ghost"
+          size="icon"
+          onClick={toggleSidebar}
+          title="Toggle Chip Menu"
+        >
+          {open ? <ChevronLeft className="size-6" /> : <ChevronRight className="size-6" />}
+        </SidebarTrigger>
+      </TooltipTrigger>
+      <TooltipContent side="right" className="font-mono text-base">
+        {open ? "Hide Chips Menu" : "Show Chips Menu"}
+      </TooltipContent>
+    </Tooltip>
   );
 }
